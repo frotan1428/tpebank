c06a72407eee6dee348ab1d9a051e4f4e8a095fc
MFROTAN <98325864+frotan1428@users.noreply.github.com>
2022-07-21 21:27:44 +0430

    v2

--- src/main/java/com/tpebank/controller/ContactMessageController.java
+++ src/main/java/com/tpebank/controller/ContactMessageController.java
@@ -1,8 +1,14 @@
 package com.tpebank.controller;
 import com.tpebank.domain.ContactMessage;
 import com.tpebank.dto.ContactMessageDTO;
+import com.tpebank.dto.response.TpeResponse;
 import com.tpebank.service.ContactMessageService;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,12 +35,10 @@ public class ContactMessageController {
        localhost:8080/message/visitor
        */
     @PostMapping("/visitor")
-    public ResponseEntity<Map<String,String>> createMessage(@Valid @RequestBody ContactMessageDTO contactMessageDTO){
+    public ResponseEntity<TpeResponse> createMessage(@Valid @RequestBody ContactMessageDTO contactMessageDTO){
         contactMessageService.saveMessage(contactMessageDTO);
-        Map<String,String> map=new HashMap<>();
-        map.put("message","Message Saved Successfully");
-        map.put("success","true");
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
+        TpeResponse tResponse=new TpeResponse(true,"Message Saved Successfully");
+        return new ResponseEntity<>(tResponse, HttpStatus.CREATED);
     }
     //localhost:8080/message
     @GetMapping
@@ -42,13 +46,44 @@ public class ContactMessageController {
         List<ContactMessageDTO> messages= contactMessageService.getAll();
         return ResponseEntity.ok(messages);
     }
-
-
+    //localhost:8080/message/1
     @GetMapping("/{id}")
     public ResponseEntity<ContactMessageDTO> getMessage(@PathVariable Long id){
         ContactMessageDTO messageDTO= contactMessageService.getMessage(id);
         return ResponseEntity.ok(messageDTO);
     }
-
-
+    //localhost:8080/message/1
+    @DeleteMapping("/{id}")
+    public ResponseEntity<TpeResponse> deleteMessage(@PathVariable Long id){
+        contactMessageService.deleteMessage(id);
+        TpeResponse tResponse=new TpeResponse(true,"Message Deleted Successfully");
+        return ResponseEntity.ok(tResponse);
+    }
+    /*
+     /*
+    {
+        "name":"Bruce",
+            "email":"Bruce@email.com",
+            "phoneNumber":"555-444-5555",
+            "subject":"Hi, What is up",
+            "body":"I have a problem about transaction"
+    }
+    localhost:8080/message/1
+    */
+    @PutMapping("/{id}")
+    public ResponseEntity<TpeResponse> updateMessage(@PathVariable Long id,@Valid @RequestBody ContactMessageDTO contactMessageDTO ){
+        contactMessageService.updateMessage(id,contactMessageDTO);
+        TpeResponse tResponse=new TpeResponse(true,"Message Updated Successfully");
+        return ResponseEntity.ok(tResponse);
+    }
+    //http://localhost:8080/message/pages?page=0&size=2&sort=id&direction=ASC
+    @GetMapping("/pages")
+    public ResponseEntity<Page<ContactMessageDTO>> getMessagePage(@RequestParam ("page") int page,
+                                                                  @RequestParam("size") int size,
+                                                                  @RequestParam("sort") String prop,
+                                                                  @RequestParam("direction") Sort.Direction direction){
+        Pageable pageable= PageRequest.of(page,size,Sort.by(direction,prop));
+        Page<ContactMessageDTO> messagePage= contactMessageService.getMessagePage(pageable);
+        return ResponseEntity.ok(messagePage);
+    }
 }
--- src/main/java/com/tpebank/service/ContactMessageService.java
+++ src/main/java/com/tpebank/service/ContactMessageService.java
@@ -6,8 +6,12 @@ import com.tpebank.exception.message.ExceptionMessages;
 import com.tpebank.repository.ContactMessageRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import javax.naming.CompositeName;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 @Service
 @AllArgsConstructor
@@ -30,11 +34,35 @@ public class ContactMessageService {
         List<ContactMessage> messages=contactMessageRepository.findAll();
         return messages.stream().map(this::convertToDTO).collect(Collectors.toList());
     }
-
-    public ContactMessageDTO getMessage(Long id){
-        ContactMessage contactMessage= contactMessageRepository.findById(id).
+    private ContactMessage getById(Long id){
+        return   contactMessageRepository.findById(id).
                 orElseThrow(()->new ResourceNotFoundException(String.format(ExceptionMessages.CONTACTMESSAGE_NOT_FOUND_MESSAGE,id)));
+    }
+    public ContactMessageDTO getMessage(Long id){
+        ContactMessage contactMessage= getById(id);
         return convertToDTO(contactMessage);
     }
-
+    public void deleteMessage(Long id){
+        ContactMessage contactMessage= getById(id);
+        contactMessageRepository.deleteById(contactMessage.getId());
+    }
+    public void updateMessage(Long id, ContactMessageDTO contactMessageDTO){
+        ContactMessage foundMessage= getById(id);
+        foundMessage.setName(contactMessageDTO.getName());
+        foundMessage.setEmail(contactMessageDTO.getEmail());
+        foundMessage.setPhoneNumber(contactMessageDTO.getPhoneNumber());
+        foundMessage.setSubject(contactMessageDTO.getSubject());
+        foundMessage.setBody(contactMessageDTO.getBody());
+        contactMessageRepository.save(foundMessage);
+    }
+    public Page<ContactMessageDTO> getMessagePage(Pageable pageable){
+        Page<ContactMessage> messages=  contactMessageRepository.findAll(pageable);
+        Page<ContactMessageDTO> dtoPage=  messages.map(new Function<ContactMessage, ContactMessageDTO>() {
+            @Override
+            public ContactMessageDTO apply(ContactMessage contactMessage) {
+                return convertToDTO(contactMessage);
+            }
+        });
+        return dtoPage;
+    }
 }
